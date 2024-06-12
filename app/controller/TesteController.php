b91dd3f47cbffbca380b901bd4c54870b6a62b5b
Higino Denilson Januário <Higino502011@hotmail.com>
2024-06-12 23:07:05 +0100

    Sgunda Subida

---
+++ app/controller/TesteController.php
@@ -0,0 +1,11 @@
+<?php 
+    namespace app\controller;
+
+    class TesteControoler{
+
+        public function __construct()
+        {   
+            return ['nome'=>'Higino'];
+        }
+
+    }
